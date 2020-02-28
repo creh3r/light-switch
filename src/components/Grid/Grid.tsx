768bbd0a125eeba8a883e4fe71262129210556c9
Christoph Reher <christoph.reher@googlemail.com>
2020-02-28 11:02:08 +0100

    Add comments to grid and game data

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -6,6 +6,13 @@ type GridProps = {
   clickHandler: (coord: number[]) => void,
 };
 
+/**
+ * Render a grid row
+ *
+ * @param row - The current row
+ * @param rowIndex - The index of the current row
+ * @param clickHandler - The click handler to pass to a tile
+ */
 function renderRow(row: number[], rowIndex: number, clickHandler: (coord: number[]) => void) {
   return (
     <div className="grid__row" key={rowIndex.toString()}>
--- src/containers/Game/data.ts
+++ src/containers/Game/data.ts
@@ -1,3 +1,15 @@
+/**
+ * Grid schema
+ *
+ *   C C C C C
+ * R 0 0 0 0 0
+ * R 0 1 1 1 0
+ * R 0 1 0 1 0
+ * R 0 1 1 1 0
+ * R 0 0 0 0 0
+ */
+
+// Empty grid ([row[col]])
 const INIT = [
   [0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0],
@@ -6,6 +18,7 @@ const INIT = [
   [0, 0, 0, 0, 0],
 ];
 
+// Game levels ([row[col]])
 const LEVEL = [
   [
     [0, 0, 0, 0, 0],
@@ -33,7 +46,6 @@ const LEVEL = [
 const START_TITLE = 'switches';
 const RUNNING_TITLE = 'tap a switch';
 const VICTORY_TITLE = 'victory';
-
 const ABORT_TITLE = 'abort';
 const RESTART_TITLE = 'restart';
 
