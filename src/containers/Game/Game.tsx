d1ad2d2711239ff3990d4fa72c019854240f0468
Christoph Reher <christoph.reher@googlemail.com>
2020-02-28 08:46:26 +0100

    Add game logic to game container

--- src/containers/Game/Game.tsx
+++ src/containers/Game/Game.tsx
@@ -1,22 +1,122 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import Grid from '../../components/Grid';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Button from '../../components/Button';
+import Data from './data';
 
 /**
- * Render the game container
+ * Render the game board
  */
-const Game = () => (
-  <div className="game">
-    <Header title="Header">
-      <Button title="Reset" clickHandler={() => {}} />
-    </Header>
-    <Grid data={[[0, 1], [1, 0]]} clickHandler={() => {}} />
-    <Footer>
-      <Button title="New game" clickHandler={() => {}} />
-    </Footer>
-  </div>
-);
+const Game = () => {
+  const [gridData, setGridData] = useState(Data.INIT);
+  const [gameTitle, setGameTitle] = useState(Data.START_TITLE);
+  const [gameInProgress, setGameInProgress] = useState(false);
+  const [gameWon, setGameWon] = useState(false);
+  const [clicks, setClicks] = useState(0);
+
+  /**
+   * Get a random grid from data
+   */
+  function getRandomGrid() {
+    return Data.LEVEL[Math.floor(Data.LEVEL.length * Math.random())];
+  }
+
+  /**
+   * Reset the game
+   */
+  const resetGame = useCallback(() => {
+    setClicks(0);
+    setGameWon(false);
+    setGameInProgress(true);
+    setGridData(getRandomGrid());
+    setGameTitle(Data.RUNNING_TITLE);
+  }, []);
+
+  /**
+   * Check if every element in array was set truthy
+   */
+  function checkIfGameWon(currentGrid: number[][]) {
+    const gridFlattened = currentGrid.flat();
+    if (!(gridFlattened.some((v) => v !== gridFlattened[0]))) {
+      setGameWon(true);
+      setGameTitle(Data.VICTORY_TITLE);
+    }
+  }
+
+  /**
+   * Handle click on grid items
+   */
+  const clickTile = useCallback((coord: number[]) => {
+    if (!gameWon && gameInProgress) {
+      const row = coord[0];
+      const col = coord[1];
+      const currentGrid = gridData.map((arr) => arr.slice());
+
+      // Check if row - 1 is out of bounds
+      if (row - 1 >= 0) {
+        currentGrid[row - 1][col] = 1 - currentGrid[row - 1][col];
+      }
+
+      // Check if row + 1 is out of bounds
+      if (row + 1 <= currentGrid.length - 1) {
+        currentGrid[row + 1][col] = 1 - currentGrid[row + 1][col];
+      }
+
+      // Check if col - 1 is out of bounds
+      if (col - 1 >= 0) {
+        currentGrid[row][col - 1] = 1 - currentGrid[row][col - 1];
+      }
+
+      // Check if col + 1 is out of bounds
+      if (col + 1 <= currentGrid.length - 1) {
+        currentGrid[row][col + 1] = 1 - currentGrid[row][col + 1];
+      }
+
+      // Toggle clicked tile
+      currentGrid[row][col] = 1 - currentGrid[row][col];
+
+      // Set grid to new calculated grid
+      setGridData(currentGrid);
+
+      checkIfGameWon(currentGrid);
+
+      // Increase click count
+      setClicks((c) => c + 1);
+    }
+  }, [gridData, gameWon, gameInProgress]);
+
+  /**
+   * Handle click on header button
+   */
+  const clickHeader = useCallback(() => {
+    if (gameInProgress) {
+      resetGame();
+    }
+  }, [gameInProgress, resetGame]);
+
+  /**
+   * Handle click on footer button
+   */
+  const clickFooter = useCallback(() => {
+    resetGame();
+  }, [resetGame]);
+
+  return (
+    <div className={`game ${gameWon ? 'game--won' : ''}`}>
+      <Header title={gameTitle}>
+        {
+          gameInProgress && <Button title="Reset" clickHandler={clickHeader} theme="blank" />
+        }
+      </Header>
+      <Grid data={gridData} clickHandler={clickTile} />
+      <Footer>
+        {
+          !gameInProgress ? <Button title="New game" clickHandler={clickFooter} /> : `${clicks} buttons pressed`
+        }
+      </Footer>
+    </div>
+  );
+};
 
 export default Game;
---
+++ src/containers/Game/data.ts
@@ -0,0 +1,48 @@
+const INIT = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+];
+
+const LEVEL = [
+  [
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 1, 0, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+  ],
+  [
+    [1, 0, 0, 0, 1],
+    [0, 1, 0, 1, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0],
+    [1, 0, 0, 0, 1],
+  ],
+  [
+    [0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0],
+    [0, 0, 1, 0, 0],
+  ],
+];
+
+const START_TITLE = 'switches';
+const RUNNING_TITLE = 'tap a switch';
+const VICTORY_TITLE = 'victory';
+
+const ABORT_TITLE = 'abort';
+const RESTART_TITLE = 'restart';
+
+export default {
+  INIT,
+  LEVEL,
+  START_TITLE,
+  RUNNING_TITLE,
+  VICTORY_TITLE,
+  ABORT_TITLE,
+  RESTART_TITLE,
+};
